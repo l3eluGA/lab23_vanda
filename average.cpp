af580adaed3da6ba5f61d6c739eb493c504ae2e1
l3eluGA <rungthiporange@gmail.com>
2023-03-03 15:16:19 +0700

    done

--- average.cpp
+++ average.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(int argc,char* argv[])
+{
+    int a=0,avg;
+    if(argc<=1)
+    {
+        cout <<"Please input numbers to find average.\n";
+    }
+    else
+    {
+        cout <<"---------------------------------\n";
+        for(int i=1;i<argc;i++)
+        {
+            a+=atoi(argv[i]);
+        }
+        avg=a/(argc-1);
+        cout <<"Average of "<<argc-1<<" numbers = "<<avg<<"\n";
+        cout <<"---------------------------------";
+    
+    }
+    
+}
